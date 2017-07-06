8dff29acc632ec697a445e0aef3a82d2f447d8d7
Yancy <zhanghongtao0402@gmail.com>
2017-07-06 23:44:28 +0800

    webpack

--- webpack-demo/src/script/Greeter.js
+++
@@ -1,7 +0,0 @@
-ar config = require('./config.json');
-
-module.exports = function() {
-  var greet = document.createElement('div');
-  greet.textContent = config.greetText;
-  return greet;
-};
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,25 +1,60 @@
+var htmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+var webpack = require('webpack');
 module.exports = {
-  devtool: 'eval-source-map',
-  entry:{
-    main : './src/script/main.js',
-    a :'./src/script/a.js'
+  context: __dirname,
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].bundle.js'
   },
-  output:{
-    path:__dirname+'/dist/js',
-    filename:'[name].js'
-  },
-  module: {//在配置文件里添加JSON loader
-    loaders: [
+  module: {
+    rules: [{
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
       {
-        test: /\.json$/,
-        loader: "json"
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: path.resolve(__dirname, 'node_modules'),
+        include: path.resolve(__dirname, 'src'),
+        query: {
+          "presets": ["latest"]
+        }
+      },
+      {
+        test: /\.css$/,
+        // loader: 'style-loader!css-loader!postcss-loader'
+        use: [
+          'style-loader', 'css-loader?importLoaders=1', 'postcss-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader', 'postcss-loader', 'less-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', 'postcss-loader', 'sass-loader'
+        ]
       }
     ]
   },
+
+  plugins: [
+    new htmlWebpackPlugin({
+      filename: "index.html",
+      template: "index.html",
+      inject: 'body'
+    })
+  ],
   devServer: {
-   contentBase: "./public",//本地服务器所加载的页面所在的目录
-   colors: true,//终端中输出结果为彩色
-   historyApiFallback: true,//不跳转
-   inline: true//实时刷新
- }
+    contentBase: "./dist", //本地服务器所加载的页面所在的目录
+    colors: true, //终端中输出结果为彩色
+    historyApiFallback: true, //不跳转
+    inline: true //实时刷新
+  }
 }
